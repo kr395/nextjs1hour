dcca3b73f0f3ea29ca5db7ee5b886fd21314e1f6
kr395 <krgopalkumar706@gmail.com>
2025-01-22 19:36:07 +0530

    counter and greet are by default server components

---
+++ app/components/counter.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react"
+
+export const Counter = ()=>{
+  const [count, setCount] = useState(0)
+  return (
+    <>
+    <button onClick={()=>{setCount(count + 1)}}>Clickec {count} times</button>
+    </>
+  )
+}
---
+++ app/components/greet.tsx
@@ -0,0 +1,9 @@
+export const Greet = () =>{
+    console.log("Hello World");
+    return (
+        <>
+          <h1>Hello there</h1>
+        </>
+    )
+    
+}
